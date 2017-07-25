c88532edee7ae0174f9cfec7cc3ffecc717cfa59
vilisimo <vilisimo@gmail.com>
2017-07-25 20:31:56 +0300

    Fix search to return -1 on searching empty collection

--- binary-search/python/binarySearch.py
+++ binary-search/python/binarySearch.py
@@ -1,6 +1,19 @@
-def search(collection, element):
-    if not collection or not element:
+# Note: for repeating numbers, this algorithm will not
+# always be correct. In order to make it so, one would
+# need to include additional steps to ensure that the
+# found value is lower than the one before it, and, if
+# necessary, find the first repeating value (for example,
+# iterating backwards until it is found)
+
+def search(collection, target):
+    try:
+        if len(collection) == 0:
+            return -1
+    except TypeError:
         raise TypeError("Arguments cannot be of type None")
+    
+    if not target:
+        raise TypeError("Arguments cannot be of type None")  
 
     low = 0
     high = len(collection) - 1
@@ -9,9 +22,9 @@ def search(collection, element):
         middle = (low + high) // 2
         guess = collection[middle]
         
-        if (guess == element):
+        if (guess == target):
             return middle
-        elif (collection[middle] > element):
+        elif (collection[middle] > target):
             high = middle - 1
 
         else: 
--- binary-search/python/test.py
+++ binary-search/python/test.py
@@ -5,52 +5,58 @@ class TestBinarySearch(unittest.TestCase):
 
     def test_invalid_collection(self):
         with self.assertRaises(TypeError):
-            search(collection=None, element=1)
-
-    def test_empty_collection(self):
-        with self.assertRaises(TypeError):
-            search(collection=[], element=1)
+            search(collection=None, target=1)
 
     def test_invalid_element(self):
         with self.assertRaises(TypeError):
-            search(collection=[1, 2, 3], element=None)
+            search(collection=[1, 2, 3], target=None)
 
-    def test_non_existent_element(self):
-        numbers = [1, 2, 3, 4]
-        actual = search(collection=numbers, element=5)
+    def test_empty_collection(self):
+        actual = search(collection=[], target=1)
 
         self.assertEqual(-1, actual)
 
+    def test_non_existent_element(self):
+        numbers = [1, 2, 3, 4]
+        actual = search(collection=numbers, target=5)
+
+        self.assertEqual(-1, actual)
 
     def test_basic_search(self):
         numbers = [1, 2, 3, 4]
-        actual = search(collection=numbers, element=3)
+        actual = search(collection=numbers, target=3)
 
         self.assertEqual(2, actual)
 
     def test_cornercase_low(self):
         numbers = [1, 2, 3, 4]
-        actual = search(collection=numbers, element=1)
+        actual = search(collection=numbers, target=1)
 
         self.assertEqual(0, actual)
 
     def test_cornercase_high(self):
         numbers = [3, 11, 27, 41]
-        actual = search(collection=numbers, element=41)
-
-        self.assertEqual(3, actual)
-
-    def test_letters(self):
-        letters = ['a', 'c', 'f', 'j', 'r']
-        actual = search(collection=letters, element='j')
+        actual = search(collection=numbers, target=41)
 
         self.assertEqual(3, actual)
 
     def test_tuples(self):
         numbers = (1, 4, 99, 101)
-        actual = search(collection=numbers, element=4)
+        actual = search(collection=numbers, target=4)
 
         self.assertEqual(1, actual)
+
+    def test_repeating_numbers(self):
+        numbers = [1, 3, 3, 6]
+        actual = search(collection=numbers, target=3)
+
+        self.assertEqual(1, actual)
+
+    def test_letters(self):
+        letters = ['a', 'c', 'f', 'j', 'r']
+        actual = search(collection=letters, target='j')
+
+        self.assertEqual(3, actual)
 
 
 if __name__ == '__main__':
