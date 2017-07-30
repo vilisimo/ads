2958ebca1f3348eb1f6c051c7a9801b0f9556ff0
Simonas Viliunas <vilisimo@gmail.com>
2017-07-30 20:28:25 +0300

    Add selection sort in Python

--- README.md
+++ README.md
@@ -4,3 +4,6 @@
 * [Binary Search](../../tree/master/algorithms/searching/binary/)
 * [Binary Search (recursive)](../../tree/master/algorithms/searching/recursive/binary/)
 * [Linear Search](../../tree/master/algorithms/searching/linear/)
+
+## Sorting Algorithms
+* [Selection Sort](../../tree/master/algorithms/sorting/selection/)
---
+++ algorithms/sorting/selection/python/selection.py
@@ -0,0 +1,13 @@
+def sort(items): 
+    if items is None:
+        raise TypeError("Arguments cannot be of type None")
+    
+    for index in range(len(items)):
+        min_index = index;
+
+        for inner_index in range(index + 1, len(items)):
+            if items[inner_index] < items[min_index]:
+                min_index = inner_index;
+        
+        if min_index != index:
+            items[index], items[min_index] = items[min_index], items[index]
