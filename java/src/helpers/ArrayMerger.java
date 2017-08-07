fde7c37bbbd7a64dd6c08c22ddf92a5c06f1a1a7
Simonas Viliunas <vilisimo@gmail.com>
2017-08-07 23:39:39 +0300

    Add array merging utility

---
+++ java/src/helpers/ArrayMerger.java
@@ -0,0 +1,24 @@
+package helpers;
+
+import java.lang.reflect.Array;
+
+public class ArrayMerger {
+
+    @SuppressWarnings("unchecked")
+    public static <T> T[] mergeArrays(T[]... arrays) {
+        int size = 0;
+        for (T[] array : arrays) {
+            size += array.length;
+        }
+
+        T[] merged = (T[]) Array.newInstance(arrays[0].getClass().getComponentType(), size);
+
+        int startIndex = 0;
+        for (T[] array : arrays) {
+            System.arraycopy(array, 0, merged, startIndex, array.length);
+            startIndex += array.length;
+        }
+
+        return merged;
+    }
+}
