31495477b0add8f581644409fd71641e7f5fee17
Simonas Viliunas <vilisimo@gmail.com>
2017-08-12 14:17:54 +0300

    Add quicksort

--- python/algorithms/sorting/quicksort.py
+++ python/algorithms/sorting/quicksort.py
@@ -22,3 +22,37 @@ def sort(items):
             less.append(items[index])
 
     return sort(less) + [items[pivot]] + sort(greater)
+
+
+def quicksort(items):
+    """ In-place quicksort with random pivot """
+
+    if items is None:
+        raise TypeError("Collection cannot be of type None")
+
+    _quicksort(items, 0, len(items) - 1)
+
+
+def _quicksort(items, first, last):
+    if first >= last:
+        return
+
+    if len(items) < 2:
+        return
+
+    pivot = items[random.randint(first, last)]
+    head, tail = first, last
+
+    while head <= tail:
+        while items[head] < pivot:
+            head += 1
+        while items[tail] > pivot:
+            tail -= 1
+
+        if head <= tail:
+            items[head], items[tail] = items[tail], items[head]
+            head += 1
+            tail -= 1
+
+        _quicksort(items, first, tail)
+        _quicksort(items, head, last)
--- python/algorithms/sorting/test/test_quicksort.py
+++ python/algorithms/sorting/test/test_quicksort.py
@@ -1,6 +1,6 @@
 import unittest
 
-from algorithms.sorting.quicksort import sort
+from algorithms.sorting.quicksort import sort, quicksort
 from helpers.contrived import Contrived
 
 
@@ -54,6 +54,66 @@ class TestQuicksort(unittest.TestCase):
         collection = sort(items)
 
         self.assertListEqual(collection, expected)
+
+
+class TestQuicksortInPlace(unittest.TestCase):
+    def test_sorting_empty_list_returns_empty_list(self):
+        collection = []
+
+        quicksort(collection)
+
+        self.assertFalse(collection)
+
+    def test_almost_sorted(self):
+        collection = [0, 21, 53, 99, -3, -4, 101]
+        expected = [-4, -3, 0, 21, 53, 99, 101]
+
+        quicksort(collection)
+
+        self.assertEqual(collection, expected)
+
+    def test_none_collection_throws_exception(self):
+        with self.assertRaises(TypeError):
+            quicksort(None)
+
+    def test_one_element_collection(self):
+        collection = [-99]
+
+        quicksort(collection)
+
+        self.assertEqual(collection[0], -99)
+
+    def test_sorting_sorts_in_order(self):
+        collection = [3, 14, 1]
+        expected = [1, 3, 14]
+
+        quicksort(collection)
+
+        self.assertListEqual(collection, expected)
+
+    def test_sorting_sorts_repeating(self):
+        collection = [-41, 99, 11, -41, 2]
+        expected = [-41, -41, 2, 11, 99]
+
+        quicksort(collection)
+
+        self.assertListEqual(collection, expected)
+
+    def test_sorting_sorts_sorted(self):
+        collection = [1, 2, 3, 4, 5]
+        expected = [1, 2, 3, 4, 5]
+
+        quicksort(collection)
+
+        self.assertEqual(collection, expected)
+
+    def test_custom_objects(self):
+        items = [Contrived("a", 3), Contrived("a", 1), Contrived("b", 13)]
+        expected = [Contrived("a", 1), Contrived("a", 3), Contrived("b", 13)]
+
+        quicksort(items)
+
+        self.assertListEqual(items, expected)
 
 
 if __name__ == '__main__':
