9d14035394cc6d557662baebc1beb68447f8af23
Simonas Viliunas <vilisimo@gmail.com>
2017-08-22 21:31:02 +0300

    Introduce Comparer

---
+++ java/test/helpers/ComparerTest.java
@@ -0,0 +1,53 @@
+package helpers;
+
+import org.junit.Test;
+
+import static helpers.Comparer.equal;
+import static helpers.Comparer.greater;
+import static helpers.Comparer.less;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class ComparerTest {
+
+    @Test
+    public void bIsGreaterThanA() {
+        assertTrue(greater("b", "a"));
+    }
+
+    @Test
+    public void aIsNotGreaterThanB() {
+        assertFalse(greater("a", "b"));
+    }
+
+    @Test
+    public void aIsNotGreaterThanA() {
+        assertFalse(greater("a", "a"));
+    }
+
+    @Test
+    public void aIsEqualA() {
+        assertTrue(equal("a", "a"));
+    }
+
+    @Test
+    public void aIsNotEqualB() {
+        assertFalse(equal("a", "b"));
+    }
+
+    @Test
+    public void aIsLessThanB() {
+        assertTrue(less("a", "b"));
+    }
+
+    @Test
+    public void bIsNotLessThanA() {
+        assertFalse(less("b", "a"));
+    }
+
+    @Test
+    public void bIsNotLessThanB() {
+        assertFalse(less("b", "b"));
+    }
+}
