2f68b0cde2048b9126ef58b651c9c2ada191a71e
vilisimo <vilisimo@gmail.com>
2019-03-17 22:11:38 +0200

    Add a boolean 2D array => table converter

---
+++ java/src/main/java/com/vilisimo/ads/algorithms/transforming/ArrayVisualiser.java
@@ -0,0 +1,45 @@
+package com.vilisimo.ads.algorithms.transforming;
+
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class ArrayVisualiser {
+
+    private static final String TRUE = "*";
+    private static final String FALSE = " ";
+
+    private ArrayVisualiser() {
+        throw new AssertionError("The class should not be instantiated");
+    }
+
+    /**
+     * Transforms contents of two dimensional boolean array into a table,
+     * where "*" represents true and " " (empty space) represents false.
+     *
+     * @param input 2D array to be represented as table
+     * @return a String representation of 2D array
+     */
+    public static String toTable(boolean[][] input) {
+        if (input.length == 0 || input[0].length == 0) {
+            return "";
+        }
+
+        var width = input[0].length;
+        var height = input.length;
+
+        var header = " " + IntStream.range(0, width)
+                .mapToObj(String::valueOf)
+                .collect(Collectors.joining());
+
+        var rows = "";
+        for (var h = 0; h < height; h++) {
+            var row = "" + h;
+            for (var w = 0; w < width; w++) {
+                row += input[h][w] ? TRUE : FALSE;
+            }
+            rows += "\n" + row;
+        }
+
+        return header + rows;
+    }
+}
---
+++ java/src/test/java/com/vilisimo/ads/algorithms/transforming/ArrayVisualiserTest.java
@@ -0,0 +1,52 @@
+package com.vilisimo.ads.algorithms.transforming;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class ArrayVisualiserTest {
+
+    @Test
+    public void transformsTwoRowsThreeColumnsToTable() {
+        //given
+        var input = new boolean[2][3];
+        input[0][0] = true;
+        input[0][1] = false;
+        input[0][2] = true;
+        input[1][0] = true;
+        input[1][1] = true;
+        input[1][2] = true;
+
+        //when
+        var result = ArrayVisualiser.toTable(input);
+
+        assertThat(result, is(" 012\n0* *\n1***"));
+    }
+
+    @Test
+    public void transformsThreeRowsTwoColumnsToTable() {
+        //given
+        var input = new boolean[3][2];
+
+        //when
+        var result = ArrayVisualiser.toTable(input);
+
+        assertThat(result, is(" 01\n0  \n1  \n2  "));
+    }
+
+    @Test
+    public void doesNotConstructTablesForZeroWidthHeightArrays() {
+        //given
+        var zeroWidth = new boolean[2][0];
+        var zeroHeight = new boolean[0][2];
+
+        //when
+        var wTable = ArrayVisualiser.toTable(zeroWidth);
+        var hTable = ArrayVisualiser.toTable(zeroHeight);
+
+        //then
+        assertThat(wTable, is(""));
+        assertThat(hTable, is(""));
+    }
+}
