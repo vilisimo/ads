80d454839a77f16a968405d1072f30f7b616f3d0
vilisimo <vilisimo@gmail.com>
2022-11-28 20:11:14 +0200

    Add missing word

--- python/leetcode/easy/ex0121.py
+++ python/leetcode/easy/ex0121.py
@@ -17,8 +17,9 @@
 #   Output: 0
 # Explanation: In this case, no transactions are done and the max profit = 0.
 
-# 1 <= prices.length <= 10^5
-# 0 <= prices[i] <= 10^4
+# Constraints:
+#   1 <= prices.length <= 10^5
+#   0 <= prices[i] <= 10^4
 
 from typing import List
 
