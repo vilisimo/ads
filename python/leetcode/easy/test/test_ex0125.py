7d244f6fb8aa9e43919090cf65e65bc859bb1d01
vilisimo <vilisimo@gmail.com>
2022-12-04 21:35:03 +0200

    Add valid palindrome solution with single loop

--- python/leetcode/easy/test/test_ex0125.py
+++ python/leetcode/easy/test/test_ex0125.py
@@ -1,6 +1,6 @@
 import pytest
 
-from leetcode.easy.ex0125 import InitialSolution
+from leetcode.easy.ex0125 import InitialSolution, SolutionWithoutLoop
 
 
 @pytest.mark.parametrize("chars, expected", [
@@ -17,3 +17,19 @@ from leetcode.easy.ex0125 import InitialSolution
 ])
 def test_recognizes_palindrome(chars: str, expected: bool):
     assert InitialSolution().isPalindrome(chars) == expected
+
+
+@pytest.mark.parametrize("chars, expected", [
+    ("A man, a plan, a canal: Panama", True),
+    ("amanaplanacanalpanama", True),
+    ("race a car", False),
+    ("sedek uzu kedes", True),
+    ("arbaabra", True),
+    ("", True),
+    (" ", True),
+    ("l", True),
+    ("fortune", False),
+    ("karnmrak", False),
+])
+def test_recognizes_palindrome_in_solution_without_loops(chars: str, expected: bool):
+    assert SolutionWithoutLoop().isPalindrome(chars) == expected
