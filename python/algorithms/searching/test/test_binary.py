ecf583d5ead45a5b2b24c8203a48378d6d51ce22
vilisimo <vilisimo@gmail.com>
2024-03-23 22:12:21 +0200

    Rewrite with pytest

--- python/algorithms/searching/test/test_binary.py
+++ python/algorithms/searching/test/test_binary.py
@@ -1,72 +1,76 @@
-import unittest
+import pytest
 
 from algorithms.searching.binary import search
 from helpers.contrived import Contrived
 
 
-class TestBinarySearch(unittest.TestCase):
-
-    def test_invalid_collection(self):
-        with self.assertRaises(TypeError):
-            search(collection=None, target=1)
-
-    def test_invalid_element(self):
-        with self.assertRaises(TypeError):
-            search(collection=[1, 2, 3], target=None)
-
-    def test_empty_collection(self):
-        actual = search(collection=[], target=1)
-
-        self.assertEqual(-1, actual)
-
-    def test_non_existent_element(self):
-        numbers = [1, 2, 3, 4]
-        actual = search(collection=numbers, target=5)
-
-        self.assertEqual(-1, actual)
-
-    def test_basic_search(self):
-        numbers = [1, 2, 3, 4]
-        actual = search(collection=numbers, target=3)
-
-        self.assertEqual(2, actual)
-
-    def test_corner_case_low(self):
-        numbers = [1, 2, 3, 4]
-        actual = search(collection=numbers, target=1)
-
-        self.assertEqual(0, actual)
-
-    def test_corner_case_high(self):
-        numbers = [3, 11, 27, 41]
-        actual = search(collection=numbers, target=41)
-
-        self.assertEqual(3, actual)
-
-    def test_tuples(self):
-        numbers = (1, 4, 99, 101)
-        actual = search(collection=numbers, target=4)
-
-        self.assertEqual(1, actual)
-
-    def test_repeating_numbers(self):
-        numbers = [1, 3, 3, 6]
-        actual = search(collection=numbers, target=3)
-
-        self.assertEqual(1, actual)
-
-    def test_letters(self):
-        letters = ['a', 'c', 'f', 'j', 'r']
-        actual = search(collection=letters, target='j')
-
-        self.assertEqual(3, actual)
-
-    def test_custom_objects(self):
-        items = [Contrived("a", 1), Contrived("b", 2), Contrived("c", 3)]
-        actual = search(collection=items, target=Contrived("b", 2))
-
-        self.assertEqual(1, actual)
+def test_invalid_collection():
+    with pytest.raises(TypeError):
+        search(collection=None, target=1)
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_invalid_element():
+    with pytest.raises(TypeError):
+        search(collection=[1, 2, 3], target=None)
+
+
+def test_empty_collection():
+    actual = search(collection=[], target=1)
+
+    assert actual == -1
+
+
+def test_non_existent_element():
+    numbers = [1, 2, 3, 4]
+    actual = search(collection=numbers, target=5)
+
+    assert actual == -1
+
+
+def test_basic_search():
+    numbers = [1, 2, 3, 4]
+    actual = search(collection=numbers, target=3)
+
+    assert actual == 2
+
+
+def test_corner_case_low():
+    numbers = [1, 2, 3, 4]
+    actual = search(collection=numbers, target=1)
+
+    assert actual == 0
+
+
+def test_corner_case_high():
+    numbers = [3, 11, 27, 41]
+    actual = search(collection=numbers, target=41)
+
+    assert actual == 3
+
+
+def test_tuples():
+    numbers = (1, 4, 99, 101)
+    actual = search(collection=numbers, target=4)
+
+    assert actual == 1
+
+
+def test_repeating_numbers():
+    numbers = [1, 3, 3, 6]
+    actual = search(collection=numbers, target=3)
+
+    assert actual == 1
+
+
+def test_letters():
+    letters = ['a', 'c', 'f', 'j', 'r']
+    actual = search(collection=letters, target='j')
+
+    assert actual == 3
+
+
+def test_custom_objects():
+    items = [Contrived("a", 1), Contrived("b", 2), Contrived("c", 3)]
+    actual = search(collection=items, target=Contrived("b", 2))
+
+    assert actual == 1
